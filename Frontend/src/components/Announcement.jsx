92b21a8414b3ff01086a4918be6758e5d0b99ba2
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-12-07 12:10:01 +0300

    update gitignore

--- Frontend/src/components/Announcement.jsx
+++ Frontend/src/components/Announcement.jsx
@@ -1,17 +1,18 @@
 import Typewriter from "typewriter-effect";
+
 const Announcement = () => {
   return (
-    <div className="bg-[#e9acd9] flex items-center justify-center text-white text-[18px] font-semibold h-[30px] p-6 text-2xl">
+    <div className="bg-pink-500 flex items-center justify-center text-white text-[18px] font-semibold h-[30px] p-8 text-2xl">
       <Typewriter
         options={{
           strings: [
             "✨ Welcome to Beauty Bliss – Where Elegance Meets You",
-            "💖 Indulge in Luxurious Skincare Crafted for Your Glow",
-            "🌸 Transform Your Daily Routine into a Spa-Worthy Ritual",
-            "🚚 Complimentary Shipping on Orders Over $150 – Just for You!",
-            "🔥 Limited Time Offer: Enjoy 50% OFF on Selected Beauty Essentials",
-            "💎 Elevate Your Beauty Game with Exclusive Must-Have Picks",
-            "🎁 Shop Now & Treat Yourself – You Deserve the Best!"
+            " Indulge in Luxurious Skincare Crafted for Your Glow",
+            " Transform Your Daily Routine into a Spa-Worthy Ritual",
+            "Complimentary Shipping on Orders Over $150 – Just for You!",
+            " Limited Time Offer: Enjoy 50% OFF on Selected Beauty Essentials",
+            " Shop Now & Treat Yourself – You Deserve the Best!",
+            "🎄 Special Christmas Gift: Surprise Beauty Bundle Inside!"
           ],
           autoStart: true,
           loop: true,
