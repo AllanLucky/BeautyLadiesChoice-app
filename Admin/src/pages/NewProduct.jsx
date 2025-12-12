57bd3d6a4034dfdaeb6f0a2236510f8c86950f69
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-12-12 22:35:46 +0300

    creating product page and the correcting the product page

--- Admin/src/pages/NewProduct.jsx
+++ Admin/src/pages/NewProduct.jsx
@@ -1,14 +1,14 @@
 import axios from "axios"
 import { userRequest } from "../RequestMethed";
-import { FaPlus } from "react-icons/fa";
+import { FaPlus, FaTrash } from "react-icons/fa";
 import { useState } from "react";
 
 const NewProduct = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [inputs, setIputs] = useState({});
   const [image, setImage] = useState("");
-  const [upload, setUpload] = useState("Uploading is 0%");
-  const [selectedOption, setSelectedOption] = useState({
+  const [uploading, setUploading] = useState("Uploading is 0%");
+  const [selectedOptions, setSelectedOption] = useState({
     concern: [],
     skinType: [],
     categories: []
@@ -31,11 +31,11 @@ const NewProduct = () => {
   const handleTRemoveOption = (name, value) => {
     setSelectedOption((prev) => ({
       ...prev,
-      [name]: prev[name].filter((option) => option !== value)
+      [name]: prev[name].filter((options) => options !== value)
     }));
   };
 
-  const handleIputChange = (e) =>{
+  const handleChange = (e) =>{
    setIputs((prev)=>{
     return{...prev, [e.target.value]:e.target.value}
    })
@@ -43,6 +43,26 @@ const NewProduct = () => {
 
   const handlleUpload = async (e) =>{
     e.preventDefault();
+    const data = new FormData
+    data.append("file", selectedImage);
+    data.append("upload_preset", "uploads");
+    setUploading("Uploading...")
+
+    try{
+      const uploadRes = await axios.post(
+         "https://api.cloudinary.com/v1_1/dkdx7xytz/image/upload",
+        data
+      );
+      const {url} = uploadRes.data;
+      setImage(url)
+      setUploading("uploaded 100%")
+      await userRequest.post("/products",{img:image,...inputs, ...selectedOptions})
+
+    }catch(error){
+      console.log(error);
+      setUploading("Uploading failed")
+
+    }
   }
 
   return (
@@ -63,11 +83,11 @@ const NewProduct = () => {
           <div className="space-y-6">
             {/* Product Image */}
             <div className="flex flex-col items-center">
-              <label htmlFor="file" className="font-semibold mb-3">Product Image</label>
+              <label className="font-semibold mb-3">Product Image</label>
               {!selectedImage ? (
-                <div className="border-2 border-gray-400 h-[250px] w-[250px] rounded-lg flex items-center   justify-center cursor-pointer hover:bg-gray-100 transition">
+                <label htmlFor="file" className="border-2 border-gray-400 h-[250px] w-[250px] rounded-lg flex items-center   justify-center cursor-pointer hover:bg-gray-100 transition">
                 <FaPlus className="text-4xl text-gray-600" />
-              </div>
+              </label>
               ):(
                 <img src={URL.createObjectURL(selectedImage)} alt="Product"
                 className="h-[100px] w-[100px]"
@@ -81,12 +101,15 @@ const NewProduct = () => {
               style={{ display:"none" }}
               />
             </div>
+            <span className="text-green-500 mt-2 text-2xl">Loading</span>
             {/* Product Name */}
             <div>
               <label className="block mb-2 font-semibold">Product Name</label>
               <input
                 type="text"
+                title="name"
                 placeholder="Product Name"
+                onChange={handleChange}
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-gray-400"
               />
             </div>
@@ -96,7 +119,9 @@ const NewProduct = () => {
               <label className="block mb-2 font-semibold">Product Description</label>
               <textarea
                 rows={8}
+                name="desc"
                 placeholder="Product Description"
+                onChange={handleChange}
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none resize-none focus:ring-2 focus:ring-gray-400"
               ></textarea>
             </div>
@@ -106,6 +131,8 @@ const NewProduct = () => {
               <label className="block mb-2 font-semibold">Product Original Price</label>
               <input
                 type="number"
+                name="originalPrice"
+                onChange={handleChange}
                 placeholder="KES 9000"
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-gray-400"
               />
@@ -115,6 +142,7 @@ const NewProduct = () => {
             <div>
               <label className="block mb-2 font-semibold">Discounted Price</label>
               <input
+              name="discountedPrice"
                 type="number"
                 placeholder="KES 9000"
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-gray-400"
@@ -130,6 +158,8 @@ const NewProduct = () => {
               <label className="block mb-2 font-semibold">Wholesale Price</label>
               <input
                 type="number"
+                name="wholesale Price"
+                onChange={handleChange}
                 placeholder="KES 9000"
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-gray-400"
               />
@@ -140,6 +170,8 @@ const NewProduct = () => {
               <label className="block mb-2 font-semibold">Wholesale Minimum Quantity</label>
               <input
                 type="number"
+                name="wholesaleMinimumQuantity"
+                onChange={handleChange}
                 placeholder="Min quantity"
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-gray-400"
               />
@@ -150,6 +182,8 @@ const NewProduct = () => {
               <label className="block mb-2 font-semibold">Brand</label>
               <input
                 type="text"
+                name="brand"
+                onChange={handleChange}
                 placeholder="Brand name"
                 className="w-full p-3 border border-gray-300 rounded-lg outline-none focus:ring-2 focus:ring-gray-400"
               />
@@ -177,6 +211,15 @@ const NewProduct = () => {
               </select>
             </div>
 
+            <div className="mt-2">
+              {selectedOptions.concern.map((option)=>(
+                <div key={option} className="flex items-center space-x-2">
+                  <span>{option}</span>
+                  <FaTrash className="text-red-500 cursor-pointer" onClick={()=>handleTRemoveOption("concern", option)}/>
+                </div>
+              ))}
+            </div>
+
             {/* Skin Type */}
             <div>
               <label className="block mb-2 font-semibold">Select Skin Type</label>
@@ -192,6 +235,14 @@ const NewProduct = () => {
                 <option value="sensitive">Sensitive</option>
                 <option value="normal">Normal</option>
               </select>
+              <div className="mt-2">
+              {selectedOptions.skinType.map((option)=>(
+                <div key={option} className="flex items-center space-x-2">
+                  <span>{option}</span>
+                  <FaTrash className="text-red-500 cursor-pointer" onClick={()=>handleTRemoveOption("skinType", option)}/>
+                </div>
+              ))}
+            </div>
             </div>
 
             {/* Category */}
@@ -208,10 +259,18 @@ const NewProduct = () => {
                 <option value="Toner">Toner</option>
                 <option value="Lotion">Lotion</option>
               </select>
+              <div className="mt-2">
+              {selectedOptions.categories.map((option)=>(
+                <div key={option} className="flex items-center space-x-2">
+                  <span>{option}</span>
+                  <FaTrash className="text-red-500 cursor-pointer" onClick={()=>handleTRemoveOption("categories", option)}/>
+                </div>
+              ))}
+            </div>
             </div>
 
             {/* Submit Button */}
-            <button className="bg-gray-800 text-white w-full py-3 rounded-lg font-semibold hover:bg-black transition mt-5">
+            <button className="bg-gray-800 text-white w-full py-3 rounded-lg font-semibold hover:bg-black transition mt-5" onClick={handlleUpload}>
               Create Product
             </button>
 
