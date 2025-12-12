54eefc9ae9c84420e937760f952655447b36dfb7
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-12-12 16:50:26 +0300

    feat(auth & redux): integrate user authentication, add login/register forms, and configure redux store with persistence

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,10 +1,51 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/apiCall";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      await login(dispatch, { email, password });
+      console.log(user.currentUser)
+      setLoading(false);
+      navigate("/")
+    } catch (error) {
+      setLoading(false);
+      if (error.response && error.response.data?.message) {
+        toast.error(error.response.data.message);
+      } else {
+        toast.error("An unexpected error occurred, please try again");
+      }
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4 py-10">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        pauseOnFocusLoss={true}
+        draggable={true}
+        pauseOnHover={true}
+      />
       <div className="flex flex-col md:flex-row bg-white shadow-xl rounded-lg overflow-hidden max-w-[900px] w-full">
-
         {/* LOGIN IMAGE */}
         <div className="w-full md:w-1/2 hidden md:flex items-center justify-center bg-gray-50">
           <img
@@ -28,6 +69,7 @@ const Login = () => {
                 type="email"
                 placeholder="example@gmail.com"
                 className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#d55fbb]"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
@@ -38,6 +80,7 @@ const Login = () => {
                 type="password"
                 placeholder="******"
                 className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-[#d55fbb]"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
@@ -54,8 +97,39 @@ const Login = () => {
             </div>
 
             {/* Submit */}
-            <button className="w-full py-3 bg-[#d55fbb] text-white font-semibold rounded-md transition duration-300 hover:bg-[#bf3ca4]">
-              Login
+            <button
+              type="submit"
+              className="w-full py-3 bg-[#d55fbb] text-white font-semibold rounded-md transition duration-300 hover:bg-[#bf3ca4]"
+              onClick={handleLogin}
+              disabled={loading} // disable button while loading
+            >
+              {loading ? (
+                <div className="flex items-center justify-center gap-2">
+                  <svg
+                    className="animate-spin h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v8H4z"
+                    ></path>
+                  </svg>
+                  Logging in...
+                </div>
+              ) : (
+                "Login"
+              )}
             </button>
 
             {/* Register */}
@@ -65,12 +139,11 @@ const Login = () => {
                 Sign Up
               </Link>
             </div>
-
           </form>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
